f682441b38d1bd7bb7fbf97a1e05f1cf2ade9173
ADY NU <adynu182@gmail.com>
2023-09-22 11:12:12 +0700

    Add files via upload

--- jobstreetapi.py
+++ jobstreetapi.py
@@ -1,5 +1,30 @@
 import requests
 import pandas as pd
+import time
+import sys, os
+
+print(
+'''Kode Spesialisasi Pekerjaan
+>> 501  Akuntansi/Keuangan
+>> 502  Sumber Daya Manusia/Personalia
+>> 503  Penjualan/Pemasaran
+>> 504  Seni/Media/Komunikasi
+>> 505  Pelayanan
+>> 506  Hotel/Restoran
+>> 507  Pendidikan/Pelatihan
+>> 508  Komputer/Teknologi Informasi
+>> 509  Teknik
+>> 510  Manufaktur
+>> 511  Bangunan/Kontruksi
+>> 512  Sains
+>> 513  Layanan Kesehatan
+>> 514  Lainnya'''
+)
+
+p = input("input max 5 Kode Spesialisasi (pisahkan dengan spasi) : \n")
+ksps = p.split(" ")
+kspi = [int(x) for x in ksps]
+# print(ksps, kspi)
 
 headers = {
     'authority': 'xapi.supercharge-srp.co',
@@ -27,13 +52,7 @@ json_data = {
     'query': 'query getJobs($country: String, $locale: String, $keyword: String, $createdAt: String, $jobFunctions: [Int], $categories: [String], $locations: [Int], $careerLevels: [Int], $minSalary: Int, $maxSalary: Int, $salaryType: Int, $candidateSalary: Int, $candidateSalaryCurrency: String, $datePosted: Int, $jobTypes: [Int], $workTypes: [String], $industries: [Int], $page: Int, $pageSize: Int, $companyId: String, $advertiserId: String, $userAgent: String, $accNums: Int, $subAccount: Int, $minEdu: Int, $maxEdu: Int, $edus: [Int], $minExp: Int, $maxExp: Int, $seo: String, $searchFields: String, $candidateId: ID, $isDesktop: Boolean, $isCompanySearch: Boolean, $sort: String, $sVi: String, $duplicates: String, $flight: String, $solVisitorId: String) {\n  jobs(\n    country: $country\n    locale: $locale\n    keyword: $keyword\n    createdAt: $createdAt\n    jobFunctions: $jobFunctions\n    categories: $categories\n    locations: $locations\n    careerLevels: $careerLevels\n    minSalary: $minSalary\n    maxSalary: $maxSalary\n    salaryType: $salaryType\n    candidateSalary: $candidateSalary\n    candidateSalaryCurrency: $candidateSalaryCurrency\n    datePosted: $datePosted\n    jobTypes: $jobTypes\n    workTypes: $workTypes\n    industries: $industries\n    page: $page\n    pageSize: $pageSize\n    companyId: $companyId\n    advertiserId: $advertiserId\n    userAgent: $userAgent\n    accNums: $accNums\n    subAccount: $subAccount\n    minEdu: $minEdu\n    edus: $edus\n    maxEdu: $maxEdu\n    minExp: $minExp\n    maxExp: $maxExp\n    seo: $seo\n    searchFields: $searchFields\n    candidateId: $candidateId\n    isDesktop: $isDesktop\n    isCompanySearch: $isCompanySearch\n    sort: $sort\n    sVi: $sVi\n    duplicates: $duplicates\n    flight: $flight\n    solVisitorId: $solVisitorId\n  ) {\n    total\n    totalJobs\n    relatedSearchKeywords {\n      keywords\n      type\n      totalJobs\n    }\n    solMetadata\n    suggestedEmployer {\n      name\n      totalJobs\n    }\n    queryParameters {\n      key\n      searchFields\n      pageSize\n    }\n    experiments {\n      flight\n    }\n    jobs {\n      id\n      adType\n      sourceCountryCode\n      isStandout\n      companyMeta {\n        id\n        advertiserId\n        isPrivate\n        name\n        logoUrl\n        slug\n      }\n      jobTitle\n      jobUrl\n      jobTitleSlug\n      description\n      employmentTypes {\n        code\n        name\n      }\n      sellingPoints\n      locations {\n        code\n        name\n        slug\n        children {\n          code\n          name\n          slug\n        }\n      }\n      categories {\n        code\n        name\n        children {\n          code\n          name\n        }\n      }\n      postingDuration\n      postedAt\n      salaryRange {\n        currency\n        max\n        min\n        period\n        term\n      }\n      salaryVisible\n      bannerUrl\n      isClassified\n      solMetadata\n    }\n  }\n}\n',
     'variables': {
         'keyword': '',
-        'jobFunctions': [
-            501,
-            502,
-            505,
-            508,
-            509,
-        ],
+        'jobFunctions': kspi,
         'locations': [
             32900,
             30501,
@@ -43,18 +62,12 @@ json_data = {
         'createdAt': None,
         'careerLevels': [],
         'page': 1,
-        'sort': 'createdAt',
+        'sort': 'createdAt',   #untuk sort berdasar tanggal
         'country': 'id',
         'sVi': '',
         'candidateId': 19089307,
         'solVisitorId': 'a11ab7d5-1fe8-4fb3-acfb-41ff8c716f0d',
-        'categories': [
-            '501',
-            '502',
-            '505',
-            '508',
-            '509',
-        ],
+        'categories': ksps,
         'workTypes': [],
         'userAgent': 'Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/109.0.0.0%20Safari/537.36',
         'industries': [],
@@ -79,13 +92,7 @@ def get_json():
             'query': 'query getJobs($country: String, $locale: String, $keyword: String, $createdAt: String, $jobFunctions: [Int], $categories: [String], $locations: [Int], $careerLevels: [Int], $minSalary: Int, $maxSalary: Int, $salaryType: Int, $candidateSalary: Int, $candidateSalaryCurrency: String, $datePosted: Int, $jobTypes: [Int], $workTypes: [String], $industries: [Int], $page: Int, $pageSize: Int, $companyId: String, $advertiserId: String, $userAgent: String, $accNums: Int, $subAccount: Int, $minEdu: Int, $maxEdu: Int, $edus: [Int], $minExp: Int, $maxExp: Int, $seo: String, $searchFields: String, $candidateId: ID, $isDesktop: Boolean, $isCompanySearch: Boolean, $sort: String, $sVi: String, $duplicates: String, $flight: String, $solVisitorId: String) {\n  jobs(\n    country: $country\n    locale: $locale\n    keyword: $keyword\n    createdAt: $createdAt\n    jobFunctions: $jobFunctions\n    categories: $categories\n    locations: $locations\n    careerLevels: $careerLevels\n    minSalary: $minSalary\n    maxSalary: $maxSalary\n    salaryType: $salaryType\n    candidateSalary: $candidateSalary\n    candidateSalaryCurrency: $candidateSalaryCurrency\n    datePosted: $datePosted\n    jobTypes: $jobTypes\n    workTypes: $workTypes\n    industries: $industries\n    page: $page\n    pageSize: $pageSize\n    companyId: $companyId\n    advertiserId: $advertiserId\n    userAgent: $userAgent\n    accNums: $accNums\n    subAccount: $subAccount\n    minEdu: $minEdu\n    edus: $edus\n    maxEdu: $maxEdu\n    minExp: $minExp\n    maxExp: $maxExp\n    seo: $seo\n    searchFields: $searchFields\n    candidateId: $candidateId\n    isDesktop: $isDesktop\n    isCompanySearch: $isCompanySearch\n    sort: $sort\n    sVi: $sVi\n    duplicates: $duplicates\n    flight: $flight\n    solVisitorId: $solVisitorId\n  ) {\n    total\n    totalJobs\n    relatedSearchKeywords {\n      keywords\n      type\n      totalJobs\n    }\n    solMetadata\n    suggestedEmployer {\n      name\n      totalJobs\n    }\n    queryParameters {\n      key\n      searchFields\n      pageSize\n    }\n    experiments {\n      flight\n    }\n    jobs {\n      id\n      adType\n      sourceCountryCode\n      isStandout\n      companyMeta {\n        id\n        advertiserId\n        isPrivate\n        name\n        logoUrl\n        slug\n      }\n      jobTitle\n      jobUrl\n      jobTitleSlug\n      description\n      employmentTypes {\n        code\n        name\n      }\n      sellingPoints\n      locations {\n        code\n        name\n        slug\n        children {\n          code\n          name\n          slug\n        }\n      }\n      categories {\n        code\n        name\n        children {\n          code\n          name\n        }\n      }\n      postingDuration\n      postedAt\n      salaryRange {\n        currency\n        max\n        min\n        period\n        term\n      }\n      salaryVisible\n      bannerUrl\n      isClassified\n      solMetadata\n    }\n  }\n}\n',
             'variables': {
             'keyword': '',
-            'jobFunctions': [
-                501,
-                502,
-                505,
-                508,
-                509,
-            ],
+            'jobFunctions': kspi,
             'locations': [
                 32900,
                 30501,
@@ -95,18 +102,12 @@ def get_json():
             'createdAt': None,
             'careerLevels': [],
             'page': i,
-            'sort': 'createdAt',
+            'sort': 'createdAt',    #untuk sort berdasar tanggal
             'country': 'id',
             'sVi': '',
             'candidateId': 19089307,
             'solVisitorId': 'a11ab7d5-1fe8-4fb3-acfb-41ff8c716f0d',
-            'categories': [
-                '501',
-                '502',
-                '505',
-                '508',
-                '509',
-            ],
+            'categories': ksps,
             'workTypes': [],
             'userAgent': 'Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/109.0.0.0%20Safari/537.36',
             'industries': [],
@@ -131,11 +132,6 @@ def all_jobs(pg):
     return id_all
     
 
-def save_to_xlsx(df):
-    df.to_excel(f'{jlmrow}_jobs.xlsx', index=False)
-    print('Data sudah disimpan')   
-    
-   
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def job_detail(id):  
@@ -178,43 +174,44 @@ def job_detail(id):
         row = resp['data']['jobDetail']
     else:
         job_none = []
-        nama = ""
-        jobtitle = ""
-        status = ""
-        pendidikan = ""
-        bahasa = ""
-        alamat = ""
-        loks = ""
-        gaji = ""
-        level = ""
-        pengalaman = ""
-        size = ""
-        deskripsi = ""
-        postdate = ""
-        closedate = ""
-        industri = ""
-        link = ""
+        nama = "N/A"
+        jobtitle = "N/A"
+        status = "N/A"
+        pendidikan = "N/A"
+        alamat = "N/A"
+        loks = "N/A"
+        gaji = "N/A"
+        level = "N/A"
+        pengalaman = "N/A"
+        size = "N/A"
+        deskripsi = "N/A"
+        postdate = "N/A"
+        closedate = "N/A"
+        industri = "N/A"
+        link = "N/A"
         job_none.append(
-        (nama, jobtitle, status, pendidikan, bahasa, alamat, loks, gaji, level, pengalaman, size, deskripsi, postdate, closedate, industri, link)
+        (nama, jobtitle, status, pendidikan, alamat, loks, gaji, level, pengalaman, size, deskripsi, postdate, closedate, industri, link)
         )        
         return job_none
 
     job_detail = []
     nm = row['header']['company']
-    if 'name' in nm or 'name' == None or 'name' == "":
-        name = "kosong"
+    if 'name' not in nm or nm['name'] == None or nm['name'] == "":
+        nama = "Dirahasiakan"
     else:
-        name = row['header']['company']['name']
+        nama = row['header']['company']['name']
         
-    nama = row['header']['company']['name']
     jobtitle = row['header']['jobTitle']
+    
     status = row['jobDetail']['jobRequirement']['employmentType']
+    if status == None or status == "":
+        status = "-"
+        
     pendidikan = row['jobDetail']['jobRequirement']['qualification']
-    bahasa = row['jobDetail']['jobRequirement']['languages']
     
     alamat = row['companyDetail']['companySnapshot']['nearbyLocations']
-    if alamat == None:
-        alamat = ""
+    if alamat == None or alamat == "":
+        alamat = "-"
         
     loks = []
     for e in range(0, len(row['location'])):
@@ -233,8 +230,17 @@ def job_detail(id):
     gaji = str(gaji)+" - "+str(gj)
     
     level = row['jobDetail']['jobRequirement']['careerLevel']
+    if level == None or level == "":
+        level = "-"
+        
     pengalaman = row['jobDetail']['jobRequirement']['yearsOfExperience']
+    if pengalaman == None or pengalaman == "":
+        pengalaman = "-"
+    
     size = row['companyDetail']['companySnapshot']['size']  
+    if size == None or size == "":
+        size = "-"
+        
     deskripsi = row['jobDetail']['jobDescription']
     postdate = row['jobDetail']['jobRequirement']['postedDate']
     closedate = row['jobDetail']['jobRequirement']['closingDate']  
@@ -242,17 +248,26 @@ def job_detail(id):
     if row['jobDetail']['jobRequirement']['industryValue'] is not None:
         industri = row['jobDetail']['jobRequirement']['industryValue']['label']
     else:
-        industri = ""
+        industri = "-"
     
     link = row['pageUrl']
     
     job_detail.append(
-        (nama, jobtitle, status, pendidikan, bahasa, alamat, loks, gaji, level, pengalaman, size, deskripsi, postdate, closedate, industri, link)
+        (nama, jobtitle, status, pendidikan, alamat, loks, gaji, level, pengalaman, size, deskripsi, postdate, closedate, industri, link)
     )
         
     return job_detail
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
+
+def save_to_xlsx(df):
+    subdir = sys.path[0]
+    subdir = f'{subdir}/DataExcel/'
+    wkt = time.localtime()
+    wkt = time.strftime("(%d-%m-%y, %H.%M.%S)", wkt)
+    print(wkt)
+    df.to_excel(f'{subdir}{jlmrow}_jobs_{wkt}.xlsx', index=False)
+    print('Data sudah disimpan')   
 
 if __name__ == '__main__':
     jsn = get_json()
@@ -276,7 +291,7 @@ if __name__ == '__main__':
     # print(all_id)
     
     
-    df = pd.DataFrame(all_id, columns=['Nama Perusahaan', 'Posisi', 'Status Pegawai', 'Pendidikan', 'Bahasa', 'Alamat Kantor', 'Lokasi/Penempatan', 'Gaji Range', 'Level', 'Pengalaman', 'Jumlah Karyawan', 'Deskripsi', 'Tanggal Posting', 'Dated', 'Jenis Perusahaan', 'Link'])
+    df = pd.DataFrame(all_id, columns=['Nama Perusahaan', 'Posisi', 'Status Pegawai', 'Pendidikan', 'Alamat Kantor', 'Lokasi/Penempatan', 'Gaji Range', 'Level', 'Pengalaman', 'Jumlah Karyawan', 'Deskripsi', 'Tanggal Posting', 'Dated', 'Jenis Perusahaan', 'Link'])
     print(df)
     jlmrow = len(df.axes[0])
     quit = input("Apakah Hasil Akan disimpan? Y/N : ")
